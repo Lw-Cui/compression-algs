bdaab03d36961d3ee37b78f07dbefaa494bbde58
lw <cui@hellolw.com>
2016-04-03 22:11:37 +0800

    using option parse lib

--- test/test.cpp
+++ test/test.cpp
@@ -1,39 +1,62 @@
 #include "bitheader/bitstream.hpp"
-#include "huffmanHeader/Huffman.hpp"
-#include "huffmanHeader/AdaptiveHuffman.hpp"
+#include "huffmanHeader/Compression.hpp"
+#include "cxxopts/cxxopts.hpp"
 #include <fstream>
+#include <string>
+#include <vector>
 #include <cstring>
 using namespace ca;
 using namespace std;
 using namespace bit;
+using namespace cxxopts;
 
 int main(int argc, char *argv[]) {
-	ifstream fin("test.txt", ios::in | ios::binary);
-	fin.unsetf(ios_base::skipws);
-	obstream bout("test.bin");
-	AdaptiveHuffman().compress(fin, bout);
-	fin.close();
-	bout.close();
-	ibstream bin("test.bin");
-	ofstream fout("test.raw");
-	AdaptiveHuffman().expand(bin, fout);
-	bin.close();
-	fout.close();
-	/*
-	if (argc == 4 && !strcmp(argv[1], "-")) {
-		ifstream fin(argv[2], ios::in | ios::binary);
-		fin.unsetf(ios_base::skipws);
-		obstream bout(argv[3]);
-		Huffman_algs().compress(fin, bout);
-		fin.close();
-		bout.close();
-	} else if (argc == 4 && !strcmp(argv[1], "+")) {
-		ibstream bin(argv[2]);
-		ofstream fout(argv[3], ios::out);
-		Huffman_algs().expand(bin, fout);
-		bin.close();
-		fout.close();
+	try {
+		bool compress = true;
+		bool adaptive = true;
+		Options options(argv[0], " - command line options");
+		options.add_options() 
+		("c,compress", "Compress file")
+		("e,expand", "Expand compressed file")
+		("s,static", "Use static huffman coding")
+		("a,adaptive", "Use adaptive huffman coding")
+		("i,input", "Input file", value<string>())
+		("o,output", "Output file", value<string>()->default_value("Processed.out"))
+		("h,help", "Print help")
+		; 
+		options.parse(argc, argv); 
+		if (options.count("h"))
+			cout << options.help({""}) << endl; 
+		if (!(options.count("c") ^ options.count("e")))
+			throw OptionException(
+				" You must specify one of the '--compress' or '--expand' options");
+		if (!(options.count("s") ^ options.count("a")))
+			throw OptionException(
+				" You must specify one of the '--static' or '--adaptive' options");
+		if (options.count("i")) { 
+			if (options.count("c")) {
+				ifstream fin(options["input"].as<string>(), ios::in | ios::binary);
+				fin.unsetf(ios_base::skipws);
+				obstream bout(options["output"].as<string>());
+				if (options.count("s")) Huffman_algs().compress(fin, bout);
+				else AdaptiveHuffman_algs().compress(fin, bout);
+				fin.close();
+				bout.close();
+			} else {
+				ibstream bin(options["input"].as<string>());
+				ofstream fout(options["output"].as<string>(), ios::out);
+				if (options.count("s")) Huffman_algs().expand(bin, fout);
+				else AdaptiveHuffman_algs().expand(bin, fout);
+				bin.close();
+				fout.close();
+			}
+		} else {
+			throw OptionException(" You must specify input file");
+		}
+	} catch (const OptionException &e) {
+		cout << "error parsing options: " << e.what() << endl;
+		cout << "Try \'huffman --help\' for more information." << endl;
+		exit(1);
 	}
-	*/
 	return 0;
 }
